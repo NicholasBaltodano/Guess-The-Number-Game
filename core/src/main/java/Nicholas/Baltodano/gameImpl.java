08713e1b11f1398ce2b35da86f7c9a9c4cc696ae
Nicholas Baltodano <nicholasbaltodano@gmail.com>
2018-08-21 19:42:58 -0500

    Game interface and Implemenation

--- core/src/main/java/Nicholas/Baltodano/gameImpl.java
+++ core/src/main/java/Nicholas/Baltodano/gameImpl.java
@@ -14,62 +14,87 @@ public class gameImpl implements Game {
     private int number;
     private int smallest;
     private int guess;
-    private int largest;
+    private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
 
+// Public Methods
+    @Override
+    public void reset() {
+        smallest         = 0;
+        guess            = 0;
+        remainingGuesses = guessCount;
+        biggest          = numberGenerator.getMaxNumber();
+        number           = numberGenerator.next();
+
+        log.debug("The Game has been reset");
+        log.debug("The number is {}", number);
+
+    }
+
     @Override
     public int getNumber() {
-        return 0;
+        return number;
     }
 
     @Override
     public int getGuess() {
-        return 0;
+        return guess;
     }
 
     @Override
     public void setGuess(int guess) {
-
+        this.guess = guess;
     }
 
     @Override
     public int getSmallest() {
-        return 0;
+        return smallest;
     }
 
     @Override
     public int getBiggest() {
-        return 0;
+        return biggest;
     }
 
     @Override
     public int getRemainingGuesses() {
-        return 0;
+        return remainingGuesses;
     }
 
-    @Override
-    public void reset() {
-
-    }
 
     @Override
     public void check() {
+        checkValidNumberRange();
 
-    }
+        if (validNumberRange == true)        {
+            if(guess > number) {
+                biggest = guess - 1;
+            }
+
+            if(guess < number )            {
+                smallest = guess + 1;
+            }
+        }
 
     @Override
-    public boolean isValidNumber() {
-        return false;
+    public boolean isValidNumberRange() {
+        return validNumberRange;
     }
 
     @Override
     public boolean isGameWon() {
-        return false;
+                return guess == number;
     }
 
     @Override
     public boolean isGameLost() {
-        return false;
+        return !isGameWon() && remainingGuesses <= 0;
+    }
+
+    // Private Methods
+    private void checkValidNumberRange()
+    {
+        validNumberRange = (guess >=smallest) && (guess <=biggest);
     }
 }
