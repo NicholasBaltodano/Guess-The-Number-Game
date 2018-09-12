fa3a396b010315ed8ff6d2e92452c3dd7197120b
Nicholas Baltodano <nicholasbaltodano@gmail.com>
2018-09-11 22:17:05 -0500

    Adding Lombok

--- console/src/main/java/Nicholas/Baltodano/Console/Main.java
+++ console/src/main/java/Nicholas/Baltodano/Console/Main.java
@@ -1,14 +1,14 @@
 package Nicholas.Baltodano.Console;
 
 import Nicholas.Baltodano.Config.GameConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
 
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    //private static final Logger log = LoggerFactory.getLogger(Main.class);
 
 
     public static void main(String[] args) {
