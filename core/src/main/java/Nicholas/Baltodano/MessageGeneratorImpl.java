df9a49b1a0a156adbcde3530e42ba494f9ba08cb
Nicholas Baltodano <nicholasbaltodano@gmail.com>
2018-09-13 20:40:42 -0500

    Adding Lombok implementation to MessageGeneratorImpl.java

--- core/src/main/java/Nicholas/Baltodano/MessageGeneratorImpl.java
+++ core/src/main/java/Nicholas/Baltodano/MessageGeneratorImpl.java
@@ -1,5 +1,6 @@
 package Nicholas.Baltodano;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,8 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 
 @Component
+@Slf4j
 public class MessageGeneratorImpl implements  MessageGenerator {
-
-    // Logging Constants
-    private static final Logger log =  LoggerFactory.getLogger(MessageGeneratorImpl.class);
 
     // Fields
     private final Game game;
